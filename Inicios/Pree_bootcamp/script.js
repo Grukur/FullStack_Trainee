085e3c35f85eb219a19758ef014b430809f538ee
Grukur <darold.trench@gmail.com>
2023-03-27 11:38:42 -0300

    sesion 2

---
+++ Inicios/Pree_bootcamp/script.js
@@ -0,0 +1,46 @@
+//para - for
+
+/* for (let i = 0; i <= 10; i++){
+    const element = array[i]
+    console.log(element)
+} */
+
+/* let edad = prompt('Ingrese su edad: ')
+
+if(edad >= 18){
+    alert("es mayor de edad")
+
+}else{
+    alert('es menos de edad')
+} */
+
+/* alert('Opcion 1: bebida. \nOpcion 2: Papas fritas\nOpcion 3: Completo')
+let opcion = prompt('Introdusca su opcion: ')
+
+switch (key) {
+    case '1':
+        alert('Usted ha comprado una Bebida')
+        
+    break;
+    case '2':
+        alert('Usted ha comprado una Papas fritas')
+        
+    break;
+    case '2':
+        alert('Usted ha comprado una Completo')
+        
+    break;
+
+    default:
+        alert('Opcion no valida...')
+        break;
+} */
+
+//CICLO WHILE
+
+/* let password = prompt('Ingrese su contraseña: ')
+while (password != 'password') {
+    password = prompt('Intenete nuevamente: ')
+       
+}
+alert('Acceso permitido.') */
