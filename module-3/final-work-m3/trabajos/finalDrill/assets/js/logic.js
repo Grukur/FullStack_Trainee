c24809a6b85c81f0e4a9e0fae4f1253238037d67
Grukur <darold.trench@gmail.com>
2023-04-25 11:03:10 -0400

    starting m4

--- module-3/final-work-m3/trabajos/finalDrill/assets/js/logic.js
+++ module-3/final-work-m3/trabajos/finalDrill/assets/js/logic.js
@@ -1,14 +1,32 @@
 
+/* ID Gen */
+/* import {v4 as uuidv4} from 'uuid';
+
+let myuuid = uuidv4();
+
+console.log('Your UUID is: ' + myuuid); */
+
+
+/* trate de molularizar, no se si está bien o se me fue la mano XD */
+
+
 /* declaracion de variables globales ES6 */
 let expendArray = [];
 let cash;
 
-/* guardamos presupuesto y desbloqueamos el form de gastos */
+/* Guardamos presupuesto y desbloqueamos el form de gastos */
 $('#btnBudget').click(function (event) {
     event.preventDefault()
     cash = saveBudget()
     showMoney(cash)
-    $('#expendures').removeAttr('disabled')
+    unBlock(cash)
+})
+
+/* Modifica el presupuesto */
+$('#btnMod').click(function (event) {
+    event.preventDefault()
+    cash = modificar()
+    showMoney(cash)
 })
 
 /* Guardamos los gastos en la tabla, en el array y al mismo tiempo calculamos y actualizamos los montos*/
@@ -19,44 +37,64 @@ $('#btnExpend').click(function (event) {
     addListItems(expend)
 })
 
-/* eliminamos gastos de la tabla y actualizamos los montos */
+/* Eliminamos gastos de la tabla y actualizamos los montos */
 $('#tBody').on('click', '.trash', function () {
     $(this).parent().parent().remove()
     deleteItem($(this).parent().prev().prev().text())
     subExpends()
-
 });
 
 /* Definimos nuestra clase constructora de objetos para manejar cada gasto */
 class Expends {
-    constructor(name, amount) {
+    constructor(name, amount, id) {
         this.name = name
         this.amount = amount
+        this.id = id
     }
 }
 
 /* F. para guardar el presupuesto */
 function saveBudget() {
     let budgetLock = $('#budget').val()
-    if (!/[\D]/gm.test(budgetLock) && budgetLock != '') {
+    if (!/[\D]/gm.test(budgetLock) && budgetLock != '' && budgetLock > 0) {
         let budget = parseInt(budgetLock)
         $('#budget').val('')
         return budget
     } else {
         alert('Por favor ingrese un monto valido, sin puntos ni comas y solo numeros')
         $('#budget').val('')
-        return
+        return 0
+    }
+}
+/* pendiente
+pendiente
+pendiente */
+const modificar = () => {
+    let budgetLock = $('#budget').val()
+    let cashLock = $('#cash').text().replace('$', '')
+    console.log(cashLock)
+    if (!/[\D]/gm.test(budgetLock) && budgetLock != '' && budgetLock > 0) {
+        let budgetMod = parseInt(budgetLock) + parseInt(cashLock)
+        $('#budget').val('')
+        return budgetMod
+    } else {
+        alert('Por favor ingrese un monto valido, sin puntos ni comas y solo numeros')
+        $('#budget').val('')
+        return 0
     }
 }
 
-/* Entregamos los datos de Presupuesto y Saldo */
+/* F. donde entregamos los datos de Presupuesto y Saldo */
 const showMoney = (cash = 0) => {
-    $('#cash').text(`$${cash}`)
-    $('#finalCash').text(`$${cash}`)
-    $('#loger').html('')
+    if (cash >= 0) {
+        $('#cash').text(`$${cash.toLocaleString()}`)
+        $('#finalCash').text(`$${cash.toLocaleString()}`)
+        /* $('#tBody').html('') */
+
+    }
 }
 
-/* Construimos nuestro objeto de gastos y guardamos en nuestro array de objetos */
+/* F. para construir nuestro objeto de gastos y guardamos en nuestro array de objetos */
 const saveExpends = () => {
     let nameExpLock = $('#nameExpend').val();
     let amntExpLock = $('#amountExpend').val();
@@ -72,21 +110,22 @@ const saveExpends = () => {
     }
 }
 
-/* Entregamos los gastos a nuestra tabla */
+/* F. donde entregamos los gastos a nuestra tabla */
 const addListItems = (expend) => {
     $('#tBody').html('')
     expend.forEach(item => {
         $('#tBody').append(`
             <tr>
                 <td>${item.name}</td>
-                <td>$${item.amount}</td>
+                <td>$${(item.amount).toLocaleString()}</td>
                 <td><img class="trash" src="./assets/img/trashCan.jpg" width="15px"></td>
+                <td class="d-none">${item.name}</td>
             </tr>
         `)
     })
 }
 
-/* Eliminamos los gastos de nuestro array de objetos */
+/* F. para eliminars los gastos de nuestro array de objetos */
 const deleteItem = (product) => {
     expendArray = expendArray.filter(item => {
         if (item.name != product) {
@@ -95,7 +134,7 @@ const deleteItem = (product) => {
     })
 }
 
-/* Sumamos los gastos en nuestro array de objetos y actualizamos Gastos y Saldo */
+/* F. para sumar los gastos en nuestro array de objetos y actualizamos Gastos y Saldo */
 const sumExpends = (expend) => {
     let expendsTotal = [];
     let total;
@@ -105,11 +144,15 @@ const sumExpends = (expend) => {
             return parseInt(a) + parseInt(b)
         })
     })
-    $('#sumExpend').text(`$${parseInt(total)}`)
-    $('#finalCash').text(`$${parseInt(cash - total)}`)
+    $('#sumExpend').text(`$${(total).toLocaleString()}`)
+    if(cash - total >= 0){
+        $('#finalCash').text(`$${(cash - total).toLocaleString()}`)
+    }else{
+        $('#finalCash').text(`$${(cash - total).toLocaleString()}`).css('color', 'red')
+    }
 }
 
-/* Sumamos los gastos eliminados de nuestro array de objetos y actualizamos Gastos y Saldo */
+/* F. donde sumamos los gastos eliminados de nuestro array de objetos y actualizamos Gastos y Saldo */
 const subExpends = () => {
     let expendsTotal = [];
     let total;
@@ -120,12 +163,32 @@ const subExpends = () => {
                 return parseInt(a) + parseInt(b)
             })
         })
-        $('#sumExpend').text(`$${parseInt(total)}`)
-        $('#finalCash').text(`$${parseInt(cash - total)}`)
+        $('#sumExpend').text(`$${(total).toLocaleString()}`)
+        if(cash - total >= 0){
+            $('#finalCash').text(`$${(cash - total).toLocaleString()}`).css('color', 'black')
+        }else{
+            $('#finalCash').text(`$${(cash - total).toLocaleString()}`).css('color', 'red')
+        }
     } else {
-        $('#sumExpend').text(0)
-        $('#finalCash').text(`$${parseInt(cash)}`)
+        $('#sumExpend').text(0).toLocaleString()
+        $('#finalCash').text(`$${(cash).toLocaleString()}`)
 
     }
 }
 
+/* F. para mostrar y actualizar gastos */
+/* const showExpends = (total, cash)=>{
+    $('#sumExpend').text(`$${(total).toLocaleString()}`)
+    if(cash - total >= 0){
+        $('#finalCash').text(`$${(cash - total).toLocaleString()}`)
+    }else{
+        $('#finalCash').text(`$${(cash - total).toLocaleString()}`).css('color', 'red')
+    }
+} */
+
+/* F. elimina el disbaled en el fieldset */
+const unBlock = (cash) => {
+    if (cash > 0) {
+        $('#expendures').removeAttr('disabled')
+    }
+}
