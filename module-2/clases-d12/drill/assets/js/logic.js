a1ba40ec553b85007e61abb3cc8d4d30f216c19b
Grukur <darold.trench@gmail.com>
2023-05-08 18:13:15 -0400

    ending day

--- module-2/clases-d12/drill/assets/js/logic.js
+++ module-2/clases-d12/drill/assets/js/logic.js
@@ -1,32 +1,79 @@
-/* Drill S11 */
 
-fetch("https://studio-ghibli-films-api.herokuapp.com/api")
+/* Abrimos JQuery por buena practica para capturar formularios */
+$(()=>{
+    /* Capturamos formulario de contacto */
+    $('#buttonForm').click(function(){
+        let nombreForm = $('#nombre').val();
+        let correoForm = $('#correo').val();
+        let telefonoForm = $('#telefono').val();
+        let motivoForm = $('#motivo').val();
+        let comentariosForm = $('#comment').val();
+        
+        alert(`Muchas gracias ${nombreForm}, hemos recibido su sugerencia y le enviaremos una pronta respuesta a: ${correoForm} o a su telefono ${telefonoForm}`);
 
-    .then(response => response.json())
-    .then(peliculas => {
+    })
+    /* Capturamos formulario de reserva */
+    $('#buttonReserve').click(function(){
+        let nombreForm = $('#nombre').val();
+        let correoForm = $('#correo').val();
+        let telefonoForm = $('#telefono').val();
+        let fechaForm = $('#fecha').val();
+        let horaForm = $('#hora').val();
+        let asistentesForm = $('#asistentes').val();
 
-        var movieArray = []
-        for (const key in peliculas) {
-            /* document.writeln(`<p>${peliculas[key].title}</p>`) */
-            /* dir = {[key]:peliculas[key].title} */
+        alert(`Muchas gracias ${nombreForm}, hemos agendado su reserva para el día ${fechaForm} a las ${horaForm}, le enviaremos su reserva al correo ${correoForm} lo esperamos para una mesa de ${asistentesForm} comenzales`)
 
-            movieArray.push({ 'image': peliculas[key].poster, 'title': peliculas[key].title, 'romantic': peliculas[key].hepburn, 'release': peliculas[key].release, 'director': peliculas[key].director, 'text': peliculas[key].synopsis })
-        }
-
-        $('#button').click(function () {
-            var randomKey = movieArray[Math.floor(Math.random() * movieArray.length)];
-            $('#img').attr('src', randomKey.image)
-            $('#title').text(randomKey.title)
-            $('#text').text(randomKey.text)
-            $('#titleRom').text(randomKey.romantic)
-            $('#release').text(randomKey.release)
-            $('#director').text(randomKey.director)
-        })
-
-    }).catch(error => {
-        console.error(error)
     })
 
+    /* pop up image */   
+    $(document).ready(function(){
+        $('.pop').on('click', function() {
+            $('#popTitle').text($(this).find('h5').text())
+            $('#popInfo').text($(this).find('p').text())
+            $('#popImg').attr('src', $(this).find('img').attr('src'));
+            $('#image-pop-up').show();
+        });
+        
+        $('#image-pop-up').on('click', function() {
+            $(this).hide();
+        });
+       
+    });
+})
+
+/* Mantenemos nuestra alerta en caso de un campo vacio */
+function nameForm(event){
+    alert(`El campo ${event.name} es obligatorio`)
+}
+
+
+
+
+/*
+function nameForm(event){
+    alert(`El campo ${event.name} es obligatorio`)
+}
+
+function exitoContacto(){
+    nombreForm = document.getElementById('nombre');
+    correoForm = document.getElementById('correo');
+    telefonoForm = document.getElementById('telefono');
+    motivoForm = document.getElementById('motivo');
+    comentariosForm = document.getElementById('comment')
+
+    alert(`Muchas gracias ${nombreForm.value}, hemos recibido su sugerencia y le enviaremos una pronta respuesta a: ${correoForm.value} o a su telefono ${telefonoForm.value}`)
+}
+
+function exitoReserva(){
+    nombreForm = document.getElementById('nombre');
+    correoForm = document.getElementById('correo');
+    telefonoForm = document.getElementById('telefono');
+    fechaForm = document.getElementById('fecha');
+    horaForm = document.getElementById('hora')
+    asistentesForm = document.getElementById('asistentes')
+
+    alert(`Muchas gracias ${nombreForm.value}, hemos agendado su reserva para el día ${fecha.value} a las ${hora.value}, le enviaremos su reserva al correo ${correoForm.value} lo esperamos para una mesa de ${asistentesForm.value} comenzales`)
+} */
 
 
 
